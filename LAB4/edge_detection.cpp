578483d9c1e3430bde142f304da6f88b7f9edd6f
Omar <jb18940@bristol.ac.uk>
2020-11-03 16:57:31 +0000

    hough space works

--- LAB4/edge_detection.cpp
+++ LAB4/edge_detection.cpp
@@ -6,6 +6,9 @@
 
 using namespace cv;
 
+#define MAXRADIUS 100
+#define MINRADIUS 20
+
 void Sobel(
 	cv::Mat &input, 
 	int size,
@@ -14,9 +17,34 @@ void Sobel(
     cv::Mat &yOutput,
     cv::Mat &dirOutput);
 
-void Normalisation(
-    cv::Mat &input, 
-    cv::Mat &output);
+void GaussianBlur(
+	cv::Mat &input, 
+	int size,
+	cv::Mat &blurredOutput);
+
+Mat threshold(
+    cv::Mat &input);
+
+void hough(
+    Mat &gradient, 
+    Mat &direction, 
+    int ***houghSpace,
+    int radius);
+
+int ***malloc3dArray(int dim1, int dim2, int dim3) {
+    int i, j, k;
+    int ***array = (int ***) malloc(dim1 * sizeof(int **));
+
+    for (i = 0; i < dim1; i++) {
+        array[i] = (int **) malloc(dim2 * sizeof(int *));
+	    for (j = 0; j < dim2; j++) {
+  	        array[i][j] = (int *) malloc(dim3 * sizeof(int));
+	    }
+
+    }
+    return array;
+}
+
 
 int main( int argc, char** argv ) {
 
@@ -35,46 +63,58 @@ int main( int argc, char** argv ) {
     Mat gray_image;
     cvtColor( image, gray_image, CV_BGR2GRAY );
 
-    Mat xSobel; 
-    Mat ySobel;
-    Mat magSobel; 
-    Mat dirSobel;          
-    Sobel(gray_image, 3, magSobel, xSobel, ySobel, dirSobel);
-    imwrite( "SobelXDirection2.jpg", xSobel );
-    imwrite( "SobelYDirection2.jpg", ySobel );
-    imwrite( "SobelMag2.jpg", magSobel );
-    imwrite( "SobelDir2.jpg", dirSobel );
+    Mat blurred;
+    GaussianBlur(gray_image,8,blurred);
 
-    Mat xNormalise;
-    Mat yNormalise;
-    Mat magNormalise;
-    Mat dirNormalise;
-    Normalisation(xSobel, xNormalise);
-    Normalisation(ySobel, yNormalise);
-    Normalisation(magSobel, magNormalise);
-    Normalisation(dirSobel, dirNormalise);
-    imwrite( "NormaliseX2.jpg", xNormalise );
-    imwrite( "NormaliseY2.jpg", yNormalise );
-    imwrite( "NormaliseMag2.jpg", magNormalise );
-    imwrite( "NormaliseDir2.jpg", dirNormalise );
+    Mat xSobel(image.rows,image.cols, CV_32FC1);
+    Mat ySobel(image.rows,image.cols, CV_32FC1);
+    Mat magSobel(image.rows,image.cols, CV_32FC1);
+    Mat dirSobel(image.rows,image.cols, CV_32FC1);         
+    Sobel(blurred, 3, magSobel, xSobel, ySobel, dirSobel);
+    imwrite( "SobelXDirection.jpg", xSobel );
+    imwrite( "SobelYDirection.jpg", ySobel );
+    imwrite( "SobelMag.jpg", magSobel );
+    imwrite( "SobelDir.jpg", dirSobel );
+
+    Mat resultX(image.rows,image.cols, CV_8UC1);
+    Mat resultY(image.rows,image.cols, CV_8UC1);
+    Mat resultMag(image.rows,image.cols, CV_8UC1);
+    Mat resultDir(image.rows,image.cols, CV_8UC1);
+
+    normalize(xSobel,resultX,0,255,NORM_MINMAX, CV_8UC1);
+    normalize(ySobel,resultY,0,255,NORM_MINMAX, CV_8UC1);
+    normalize(magSobel,resultMag,0,255,NORM_MINMAX);
+    normalize(dirSobel,resultDir,0,255,NORM_MINMAX);
+    imwrite("grad_x.jpg",resultX);
+    imwrite("grad_y.jpg",resultY);
+    imwrite("mag.jpg",resultMag);
+    imwrite("dir.jpg", resultDir);
+
+    int ***houghSpace = malloc3dArray(image.rows, image.cols, MAXRADIUS);
+
+    Mat testing = imread("mag.jpg", 1);
+    Mat gray_test;
+    cvtColor( testing, gray_test, CV_BGR2GRAY );
+
+    Mat thresh = threshold(gray_test);
+
+    hough(thresh, dirSobel, houghSpace, MAXRADIUS-MINRADIUS);
 
     return 0;
 }
 
 void Sobel(cv::Mat &input, int size, cv::Mat &magOutput, cv::Mat &xOutput, cv::Mat &yOutput, cv::Mat &dirOutput) {
-	// intialise the output using the input
-	magOutput.create(input.size(), input.type());
-    xOutput.create(input.size(), input.type());
-    yOutput.create(input.size(), input.type());
-    dirOutput.create(input.size(), input.type());
 
-    double kernelX[3][3] = {{1, 0, -1},
-                            {2, 0, -2},
-                            {1, 0, -1}};
+    int kernelX[3][3] = {{-1, 0, 1},
+                            {-2, 0, 2},
+                            {-1, 0, 1}};
 
-    double kernelY[3][3] = {{1, 2, 1},
+    int kernelY[3][3] = {{-1, -2, -1},
                             {0, 0, 0},
-                            {-1, -2, -1}};
+                            {1, 2, 1}};
+
+    cv::Mat kernelXMat = cv::Mat(size, size, cv::DataType<int>::type, kernelX);
+    cv::Mat kernelYMat = cv::Mat(size, size, cv::DataType<int>::type, kernelY);
 
 	// we need to create a padded version of the input
 	// or there will be border effects
@@ -89,8 +129,8 @@ void Sobel(cv::Mat &input, int size, cv::Mat &magOutput, cv::Mat &xOutput, cv::M
 	// now we can do the convoltion
 	for ( int i = 0; i < input.rows; i++ ) {	
 		for( int j = 0; j < input.cols; j++ ) {
-			double sumX = 0.0;
-            double sumY = 0.0;
+			float sumX = 0.0;
+            float sumY = 0.0;
 
 			for( int m = -kernelRadiusX; m <= kernelRadiusX; m++ ) {
 				for( int n = -kernelRadiusY; n <= kernelRadiusY; n++ ) {
@@ -102,8 +142,8 @@ void Sobel(cv::Mat &input, int size, cv::Mat &magOutput, cv::Mat &xOutput, cv::M
 
 					// get the values from the padded image and the kernel
 					int imageval = ( int ) paddedInput.at<uchar>( imagex, imagey );
-					double kernalvalX = kernelX[kernelx][kernely];
-                    double kernalvalY = kernelY[kernelx][kernely];
+					int kernalvalX = kernelXMat.at<int>(kernelx, kernely);
+                    int kernalvalY = kernelYMat.at<int>(kernelx, kernely);
 
 					// do the multiplication
 					sumX += imageval * kernalvalX;		
@@ -111,32 +151,134 @@ void Sobel(cv::Mat &input, int size, cv::Mat &magOutput, cv::Mat &xOutput, cv::M
 				}
 			}
 			// set the output value as the sum of the convolution
-			xOutput.at<uchar>(i, j) = (uchar) sumX;
-            yOutput.at<uchar>(i, j) = (uchar) sumY;
+			xOutput.at<float>(i, j) = (float) sumX;
+            yOutput.at<float>(i, j) = (float) sumY;
 
-            magOutput.at<uchar>(i, j) = (uchar) sqrt((sumX * sumX) + (sumY * sumY));
+            magOutput.at<float>(i, j) = (float) sqrt((sumX * sumX) + (sumY * sumY));
 
-            dirOutput.at<uchar>(i, j) = (uchar) atan2(sumY, sumX);
+            dirOutput.at<float>(i, j) = (float) atan2(sumY, sumX);
 		}
 	}
 }
 
-void Normalisation(cv::Mat &input, cv::Mat &output) {
-    output.create(input.size(), input.type());
-    double minVal; 
-    double maxVal; 
-    minMaxLoc( input, &minVal, &maxVal );
-    std::cout << minVal << std::endl;
-    std::cout << maxVal << std::endl;
+Mat threshold(cv::Mat &input) {
+    Mat thresh(input.rows, input.cols, CV_8UC1);
 
-    double newMin = 0;
-    double newMax = 255;
-
-    for (int i = 0; i < input.rows; i++) {
-        for (int j = 0; j < input.cols; j++) {
+    for ( int i = 0; i < input.rows; i++ ) {	
+		for( int j = 0; j < input.cols; j++ ) {
             int imageval = ( int ) input.at<uchar>( i, j );
-            double newIm = (imageval - minVal) * ((newMax - newMin)/(maxVal - minVal)) + newMin;
-            output.at<uchar>(i, j) = (uchar) newIm;
+            
+            if (imageval > 110) {
+                thresh.at<uchar>( i, j ) = 255;
+            } else {
+                thresh.at<uchar>( i, j ) = 0;
+            }
         }
     }
+    imwrite("thresh.jpg", thresh);
+
+    return thresh;
+}
+
+void hough(Mat &gradient, Mat &direction, int ***houghSpace, int radius) {
+    for (int i = 0; i < gradient.rows; i++) {
+        for (int j = 0; j < gradient.cols; j++) {
+            for (int k = 0; k < MAXRADIUS; k++) {
+                houghSpace[i][j][k] = 0;
+            }
+        }
+    }
+
+
+    for (int x = 0; x < gradient.rows; x++) {
+        for (int y = 0; y < gradient.cols; y++) {
+            if (gradient.at<uchar>(x, y) == 255) {
+                for (int r = MINRADIUS; r < MAXRADIUS; r++) {
+                    int b =  y - (r * cos(direction.at<float>(x, y)));
+                    int a =  x - (r * sin(direction.at<float>(x, y)));
+
+                    int d =  y + (r * cos(direction.at<float>(x, y)));
+                    int c =  x + (r * sin(direction.at<float>(x, y)));
+                    
+                    if (b >= 0 && b < gradient.cols && a >= 0 && a < gradient.rows) {
+                        houghSpace[a][b][r] += 1;
+                    }
+                    if (d >= 0 && d < gradient.cols && c >= 0 && c < gradient.rows) {
+                        houghSpace[c][d][r] += 1;
+                    }
+                }
+            }
+        }
+    }
+
+    Mat houghSpaceOutput(gradient.rows, gradient.cols, CV_32FC1);
+
+    for (int x = 0; x < gradient.rows; x++) {
+        for (int y = 0; y < gradient.cols; y++) {
+            for (int r = MINRADIUS; r < MAXRADIUS; r++) {
+                houghSpaceOutput.at<float>(x,y) += houghSpace[x][y][r];
+            }
+
+        }
+    }
+
+    Mat houghSpaceConvert(gradient.rows, gradient.cols, CV_8UC1);
+    normalize(houghSpaceOutput, houghSpaceConvert, 0, 255, NORM_MINMAX);
+
+    imwrite( "houghOuput.jpg", houghSpaceConvert );
+}
+
+void GaussianBlur(cv::Mat &input, int size, cv::Mat &blurredOutput)
+{
+	// intialise the output using the input
+	blurredOutput.create(input.size(), input.type());
+
+	// create the Gaussian kernel in 1D 
+	cv::Mat kX = cv::getGaussianKernel(size, -1);
+	cv::Mat kY = cv::getGaussianKernel(size, -1);
+	
+	// make it 2D multiply one by the transpose of the other
+	cv::Mat kernel = kX * kY.t();
+
+	//CREATING A DIFFERENT IMAGE kernel WILL BE NEEDED
+	//TO PERFORM OPERATIONS OTHER THAN GUASSIAN BLUR!!!
+
+	// we need to create a padded version of the input
+	// or there will be border effects
+	int kernelRadiusX = ( kernel.size[0] - 1 ) / 2;
+	int kernelRadiusY = ( kernel.size[1] - 1 ) / 2;
+
+	cv::Mat paddedInput;
+	cv::copyMakeBorder( input, paddedInput, 
+		kernelRadiusX, kernelRadiusX, kernelRadiusY, kernelRadiusY,
+		cv::BORDER_REPLICATE );
+
+	// now we can do the convoltion
+	for ( int i = 0; i < input.rows; i++ )
+	{	
+		for( int j = 0; j < input.cols; j++ )
+		{
+			double sum = 0.0;
+			for( int m = -kernelRadiusX; m <= kernelRadiusX; m++ )
+			{
+				for( int n = -kernelRadiusY; n <= kernelRadiusY; n++ )
+				{
+					// find the correct indices we are using
+					int imagex = i + m + kernelRadiusX;
+					int imagey = j + n + kernelRadiusY;
+					int kernelx = m + kernelRadiusX;
+					int kernely = n + kernelRadiusY;
+
+					// get the values from the padded image and the kernel
+					int imageval = ( int ) paddedInput.at<uchar>( imagex, imagey );
+					double kernalval = kernel.at<double>( kernelx, kernely );
+
+					// do the multiplication
+					sum += imageval * kernalval;							
+				}
+			}
+			// set the output value as the sum of the convolution
+			blurredOutput.at<uchar>(i, j) = (uchar) sum;
+		}
+	}
 }
